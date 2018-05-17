8adc08d2d57df5df697bab1be4a13d88c5c7ee7c
cdcalderon <cdcalderon@gmail.com>
2018-05-16 23:48:09 -0500

    Dummy change- for deployment

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi There, I'm a react APP </h1>
+        <h1>Hi There, Testing Component </h1>
       </div>
     );
   }
